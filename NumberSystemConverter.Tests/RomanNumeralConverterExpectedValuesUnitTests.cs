4bda63ddf1e0d3d159f3d6de8a9fcec0cb117879
Deepak Acharya <aacharyadeepak100@gmail.com>
2019-01-23 16:31:27 +0530

    Added some more test cases

--- NumberSystemConverter.Tests/RomanNumeralConverterExpectedValuesUnitTests.cs
+++ NumberSystemConverter.Tests/RomanNumeralConverterExpectedValuesUnitTests.cs
@@ -20,6 +20,41 @@ namespace NumberSystemConverter.Tests
             Assert.AreEqual("MMM", result);
         }
 
+        [TestMethod]
+        public void NumberEqual55ExpectedResultLV()
+        {
+            string result = ConvertIntToRoman(55);
+            Assert.AreEqual(result, "LV");
+        }
+
+        [TestMethod]
+        public void NumberEqual100ExpectedResultC()
+        {
+            string result = ConvertIntToRoman(100);
+            Assert.AreEqual(result, "C");
+        }
+
+        [TestMethod]
+        public void NumberEqual500ExpectedResultD()
+        {
+            string result = ConvertIntToRoman(500);
+            Assert.AreEqual(result, "D");
+        }
+
+        [TestMethod]
+        public void NumberEqual599ExpectedResultDLXXXXVIIII()
+        {
+            var result = ConvertIntToRoman(599);
+            Assert.AreEqual(result, "DLXXXXVIIII");
+        }
+
+        [TestMethod]
+        public void NumberEqual2013ExpectedResultMMXIII()
+        {
+            var result = ConvertIntToRoman(2013);
+            Assert.AreEqual(result, "MMXIII");
+        }
+
         private string ConvertIntToRoman(int num)
         {
             return new RomanNumeralConverter().ConvertRomanNumeral(num);
