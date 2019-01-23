3b218202b53b50127c72d153dfa9186f049ecbe4
Deepak Acharya <aacharyadeepak100@gmail.com>
2019-01-23 15:51:22 +0530

    Added one more test case

--- NumberSystemConverter.Tests/RomanNumeralConverter_Test.cs
+++ NumberSystemConverter.Tests/RomanNumeralConverter_Test.cs
@@ -8,7 +8,7 @@ namespace NumberSystemConverter.Tests
     {
         [TestMethod]
         [ExpectedException(typeof(IndexOutOfRangeException))]
-        public void NumberGreaterThan3000ThrowsIndexOutOfRangeException()
+        public void NumberGreaterThanThreeThousandShouldThrowIndexOutOfRangeException()
         {
             RomanNumeralConverter romanNumeralConverter = new RomanNumeralConverter();
             romanNumeralConverter.ConvertRomanNumeral(3001);
@@ -16,10 +16,18 @@ namespace NumberSystemConverter.Tests
 
         [TestMethod]
         [ExpectedException(typeof(IndexOutOfRangeException))]
-        public void NumberLessThan0ThrowsIndexOutOfRangeException()
+        public void NumberLessThanZeroShouldThrowIndexOutOfRangeException()
         {
             RomanNumeralConverter romanNumeralConverter = new RomanNumeralConverter();
             romanNumeralConverter.ConvertRomanNumeral(-1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void NumberEqualToZeroShouldThrowIndexOutOfRangeException()
+        {
+            RomanNumeralConverter romanNumeralConverter = new RomanNumeralConverter();
+            romanNumeralConverter.ConvertRomanNumeral(0);
         }
     }
 }
